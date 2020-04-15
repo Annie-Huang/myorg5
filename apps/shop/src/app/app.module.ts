e4af5ac74cfa3219a65148dcf7bcfecd4b2f84f4
Annie Huang <annie.hn@gmail.com>
2020-04-15 16:03:45 +1000

    Add router so it can go to different pages.

--- apps/shop/src/app/app.module.ts
+++ apps/shop/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UiModule } from '@myorg5/ui';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UiModule],
+  imports: [BrowserModule, UiModule, RouterModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent]
 })

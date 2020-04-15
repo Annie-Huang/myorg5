3eea32a33fde017fcfe138a84d97c34075f7960f
Annie Huang <annie.hn@gmail.com>
2020-04-15 17:58:15 +1000

    Add UiModule into feature-main.module.ts so we can use <myorg5-item-list> in feature-main

--- libs/feature-main/src/lib/feature-main.module.ts
+++ libs/feature-main/src/lib/feature-main.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { UiModule } from '@myorg5/ui';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    UiModule,
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
       {path: '', pathMatch: 'full', component: MainComponent}

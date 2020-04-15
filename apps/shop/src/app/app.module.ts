2089a7fb9990c3d30b3aca06e4f84dd24199160f
Annie Huang <annie.hn@gmail.com>
2020-04-15 11:03:37 +1000

    Notice when you do import for UiModule, it automatically do import { UiModule } from '@myorg5/ui'; This is in the @<OrgName>/<LibName> format

--- apps/shop/src/app/app.module.ts
+++ apps/shop/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UiModule } from '@myorg5/ui';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, UiModule],
   providers: [],
   bootstrap: [AppComponent]
 })

f6d42246135f3432ba6d81813595e885a9bc32e4
Annie Huang <annie.hn@gmail.com>
2020-04-15 17:47:39 +1000

    Copied  the <main> section in apps/shop/.../app.component.ts into libs/feature-main/.../main.component

--- libs/feature-main/src/lib/main/main.component.ts
+++ libs/feature-main/src/lib/main/main.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'myorg5-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent {
+  items = [
+    {
+      code: 'blanket',
+      description: 'weight blanket'
+    },
+    {
+      code: 'pillow',
+      description: 'large soft pillow'
+    },
+    {
+      code: 'mattress',
+      description: 'queen size firm mattress'
+    },
+  ];
 
-  constructor() { }
+  cart = [];
 
-  ngOnInit(): void {
+  addToCart(code: string) {
+    this.cart.push(code);
   }
 
 }

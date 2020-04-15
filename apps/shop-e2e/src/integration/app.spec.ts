d8244e552ff9e33c743342b31dfa6881a0ac6512
Annie Huang <annie.hn@gmail.com>
2020-04-15 10:28:01 +1000

    Update cypress test

--- apps/shop-e2e/src/integration/app.spec.ts
+++ apps/shop-e2e/src/integration/app.spec.ts
@@ -1,13 +1,15 @@
-import { getGreeting } from '../support/app.po';
+import { getGreeting, addItem, getShoppingCartStatus } from '../support/app.po';
 
 describe('shop', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display welcome message and the cart', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('Welcome to shop!');
+    addItem('blanket');
+    getShoppingCartStatus().contains('1 item');
   });
 });

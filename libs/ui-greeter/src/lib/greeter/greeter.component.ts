855ed214592d9a4e8ef3cd0a04cc5e5ef9340105
Annie Huang <annie.hn@gmail.com>
2020-05-02 12:48:56 +1000

    nx g component greeter --project=ui-greeter

---
+++ libs/ui-greeter/src/lib/greeter/greeter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'myorg5-greeter',
+  templateUrl: './greeter.component.html',
+  styleUrls: ['./greeter.component.css']
+})
+export class GreeterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/ui-greeter/src/lib/ui-greeter.module.ts
+++ libs/ui-greeter/src/lib/ui-greeter.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GreeterComponent } from './greeter/greeter.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [GreeterComponent]
 })
 export class UiGreeterModule {}
